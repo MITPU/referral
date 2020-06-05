097634847aeb8c615c0091c10e47970fbbdbfaa8
Enkh Erdenebat <eerdenebat@wsgc.com>
2020-06-05 08:48:11 -0700

    [9] Created a referral-core project.

---
+++ core/src/test/java/org/mitpu/referral/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mitpu.referral.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
