8d9072fbf5a133499b4268d8e6f131741dc67f1e
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-08-23 19:54:42 -0500

    Added Skills and PhoneUI

--- web/src/components/ParticipantForm/ParticipantForm.js
+++ web/src/components/ParticipantForm/ParticipantForm.js
@@ -5,8 +5,9 @@ import { Route } from 'react-router-dom';
 
 const ParticipantForm = (props) => {
     return (
-        <div className={classes.Participant}>
-            <Route render={() => (<PFS companyNames={props.companyName} />)} />
+        <div className = {classes.Participant}>
+            <Route
+            render={() => (<PFS companyNames={props.companyName} candidateSkills={props.companySkill}/>) }/>
         </div>
     );
 };
--- web/src/containers/Forms/mainFormStructure.js
+++ web/src/containers/Forms/mainFormStructure.js
@@ -5,27 +5,33 @@ import Referrer from '../../components/ReferrerForm/ReferrerForm';
 import Admin from '../../components/Admin/Admin';
 import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { fetchCOMPANIES } from '../../store/actions';
+import {fetchCOMPANIES} from '../../store/actions';
+import {fetchSKILLS} from '../../store/actions';
 
 class FormBuilder extends Component {
 
     state = {
-        companyNames: []
+        companyNames: [],
+        skills: []
     }
 
     componentDidMount() {
         const { dispatch } = this.props
         dispatch(fetchCOMPANIES())
-    }
+        dispatch(fetchSKILLS())
+    };
 
-    render() {
-        return (
+    render () {
+        return(
             <div>
                 <Aux>
                     <Switch>
                         <Route path="/referrer" component={Referrer} />
                         <Route path="/admin" component={Admin} />
-                        <Route path="/" render={() => (<Participant companyName={this.props.companyNames} />)} />
+                        <Route path="/" render={() => (<Participant 
+                            companyName={this.props.companyNames}
+                            companySkill={this.props.skills}  />)} />
+                            
                         <Route path="/login" />
                     </Switch>
                 </Aux>
@@ -35,7 +41,8 @@ class FormBuilder extends Component {
 }
 const mapStateToProps = state => {
     return {
-        companyNames: state.companyNames
+        companyNames: state.companyNames,
+        skills: state.skills
     };
 };
 
